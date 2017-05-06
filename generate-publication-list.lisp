e76221b2bc4a8d6f3b91bb9cd721ec17ee5edff7
Mark J. Nelson <mjn@anadrome.org>
2017-05-06 21:01:29 +0100

    Only link PDFs if they exist.

--- generate-publication-list.lisp
+++ generate-publication-list.lisp
@@ -5,16 +5,16 @@
 ; bibtex2web.
 
 (defvar *author-name* "Mark J. Nelson")
+(defvar *output-directory* '(:relative "publications"))
 
 (ql:quickload "spinneret")
 (ql:quickload "spinneret/cl-markdown")
 (use-package :spinneret)
 ;(ql:quickload "multival-plist")
 ;(use-package :multival-plist)
-(ql:quickload "cl-markdown")
 
 (defmacro with-page-output (filename title &body body)
-  `(with-open-file (*html* (make-pathname :directory '(:relative "publications") :name ,filename)
+  `(with-open-file (*html* (make-pathname :directory *output-directory* :name ,filename)
                            :direction :output :if-exists :supersede)
      (with-html
        (:doctype)
@@ -33,8 +33,8 @@
     (let ((last-year))
       (dolist (publication *publications*)
         (let* ((basename (citation-key-to-basename (getf publication :citation-key)))
-               (abstract-url (concatenate 'string basename "-abstract.html"))
-               (pdf-url (concatenate 'string basename ".pdf"))
+               (abstract-filename (concatenate 'string basename "-abstract.html"))
+               (pdf-filename (concatenate 'string basename ".pdf"))
                (publication-type (getf publication :publication-type))
                (authors (publication-authors publication))
                (title (getf publication :title))
@@ -50,16 +50,19 @@
             (:h2 year))
           (:p ; ToC citation format
             authors (:br)
-            (:strong (:a :href abstract-url title) (:br))
+            (:strong (:a :href abstract-filename title) (:br))
             (:em venue))
 
           ; publication's abstract page
-          (with-page-output abstract-url title
+          (with-page-output abstract-filename title
             (let ((full-venue (if (eq publication-type 'inproceedings)
                                 (concatenate 'string "Proceedings of the " venue)
                                 venue)))
               (:p ; 'normal' citation format
-                ("~a (~a). [**~a**](~a)." authors year pdf-url title)
+                ("~a (~a). " authors year)
+                (if (probe-file (make-pathname :directory *output-directory* :name pdf-filename))
+                  (:a :href pdf-filename (:strong ("~a." title)))
+                  (:strong ("~a." title)))
                 (case publication-type ((inproceedings incollection) "In"))
                 ("*~a*~a~a."
                  full-venue
